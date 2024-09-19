637360d3eb438d29e0e553525fab747c02a0ea84
Muhammad Zain Rajput <139969894+Muhammadzain82@users.noreply.github.com>
2024-09-19 15:25:46 +0500

    issues fixed

--- src/app/Dashboard/page.js
+++ src/app/Dashboard/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@radix-ui/react-tabs';
 import Nextpage from '../Leaderboard/page'
-import GoalsDashboard from '../Goals/page'
+import GoalsDashboard from '../Goals/[page]'
 import MyReceiversDashboard from '../Listing/page'
 
 export default function Dashboard() {
